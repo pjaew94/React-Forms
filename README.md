a068d0a19382b9e9e36119fe42bfc02b17893efa
pjaew94 <55684001+pjaew94@users.noreply.github.com>
2019-12-20 14:50:40 -0500

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# React-Forms
-Created with CodeSandbox
---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+
+  const [heading, setHeading] = useState("");
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleClick() {
+    setHeading(name);
+  }
+
+  return (
+    <div className="container">
+      <h1>Hello {heading}</h1>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+      />
+      <button onClick={handleClick}>Submit</button>
+    </div>
+  );
+}
+
+export default App;
